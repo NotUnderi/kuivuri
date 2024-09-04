317cc9c8f82b604e50a8cddaede5c7d6f4ba1b3f
Underi <undershotbutton@gmail.com>
2024-09-05 01:12:38 +0300

    Initial commit

---
+++ temps/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from .models import Temperature
+from django.http import HttpResponse
+import requests as r
+from django.views.generic import TemplateView
+from chartjs.views.lines import BaseLineChartView
+
+
+def index(request):
+    temps = Temperature.objects.all()
+    print(temps)
+    return render (request,'index.html',{"temps":temps})
+
+
+    
+
+
+class TempJsonView(BaseLineChartView):
+    def get_labels(self):
+        l = []
+        for i in Temperature.objects.values("time"):
+            l.append(i)
+        return l
+    
+    
+    def get_data(self):
+        l = []
+        for i in Temperature.objects.get():
+            l.append(i)
+            print(i)
+        return l
+
+
+
+line_chart = TemplateView.as_view(template_name='linechart.html')
+line_chart_json = TempJsonView.as_view()
+
+# Create your views here.
