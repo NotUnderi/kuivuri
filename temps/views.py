48cc5ff9b157bc34d8ea7bae24c5294db8945e29
NotUnderi <undershotbutton@gmail.com>
2025-01-31 15:37:34 +0200

    Fixed time axis on the chart and modified the site layout.

--- temps/views.py
+++ temps/views.py
@@ -4,6 +4,7 @@ from django.views.generic import TemplateView
 from chartjs.views.lines import BaseLineChartView
 import datetime
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 
 
@@ -36,7 +37,19 @@ class TempJsonView(BaseLineChartView):
 
 
 line_chart = TemplateView.as_view(template_name='linechart.html')
-line_chart_json = TempJsonView.as_view()
+
+def line_chart_json(request):
+    temperatures = Temperature.objects.all().order_by('time')
+    data = {
+        "labels": [temp.time.isoformat() for temp in temperatures],
+        "datasets": [{
+            "label": "Temperature",
+            "data": [temp.temp for temp in temperatures],
+            "fill": False,
+            "borderColor": "rgba(75, 192, 192, 1)",
+            "tension": 0.1
+        }]
+    }
+    return JsonResponse(data)
 
 # Create your views here.
-    
